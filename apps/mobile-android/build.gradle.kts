16a2ec38e407acf54ad4905261745b644d7c8579
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-07-07 14:05:53 +0200

    first commit

---
+++ apps/mobile-android/build.gradle.kts
@@ -0,0 +1,40 @@
+val javaVersion: String by project
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.application")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    android()
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(project(":libs:mobile-shared"))
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    namespace = "com.example.mobile"
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    defaultConfig {
+        applicationId = "com.example.mobile.Mobile"
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(javaVersion)
+        targetCompatibility = JavaVersion.toVersion(javaVersion)
+    }
+    kotlin {
+        jvmToolchain(javaVersion.toInt())
+    }
+}
